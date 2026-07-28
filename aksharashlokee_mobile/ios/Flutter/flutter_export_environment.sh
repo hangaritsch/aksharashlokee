8567e1e9bce7e1dc33a1ad1fced01d9130e205ff
HangariTech <hangaritech@gmail.com>
2026-07-28 17:59:51 +0530

    Add privacy policy index.html and .nojekyll

---
+++ aksharashlokee_mobile/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/gopal/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/opt/homebrew/var/www/aksharashlokee_sql/aksharashlokee_mobile"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/opt/homebrew/var/www/aksharashlokee_sql/aksharashlokee_mobile/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/opt/homebrew/var/www/aksharashlokee_sql/aksharashlokee_mobile/.dart_tool/package_config.json"

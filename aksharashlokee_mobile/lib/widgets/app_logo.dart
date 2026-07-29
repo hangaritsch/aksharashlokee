552677ab458d618ee3fc1f63275b5d17538d96bf
HangariTech <hangaritech@gmail.com>
2026-07-29 16:07:20 +0530

    Update logo widget and launcher icons to use a_logo.png

--- aksharashlokee_mobile/lib/widgets/app_logo.dart
+++ aksharashlokee_mobile/lib/widgets/app_logo.dart
@@ -15,7 +15,7 @@ class AppLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Image.asset(
-      'assets/logo.png',
+      'assets/a_logo.png',
       width: size,
       height: size,
       fit: BoxFit.contain,
